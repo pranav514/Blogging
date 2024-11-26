5cd5be024306e9c9505a203242a06c3c0e9bf234
pranav514 <pranavsalunkhe327@gmail.com>
2024-11-27 00:28:11 +0530

    added more comp

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import  { useState } from 'react';
 import axios from 'axios';
 import {  useNavigate } from 'react-router-dom';
-import Animation from '../components/Animation';
 function Login() {
     const [email , setEmail] = useState("");
     const [password , setPassword] = useState("");
